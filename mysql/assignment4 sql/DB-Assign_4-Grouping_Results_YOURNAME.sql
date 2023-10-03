1bb876c72e917fa70cb96c32914949d9e236d342
jaalle1 <bbo3063@gmail.com>
2023-10-03 14:56:13 -0400

    added assignment 4 sql

---
+++ mysql/assignment4 sql/DB-Assign_4-Grouping_Results_YOURNAME.sql
@@ -0,0 +1,23 @@
+--YOUR NAME HERE	ASSIGNMENT 4 GROUPING RESULTS
+--Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 5; your query for question 1B should go on line 7...
+-- 1 
+--A
+SELECT MIN(price) FROM stock_items;
+--B
+SELECT MAX(inventory) FROM stock_items;
+
+-- 2
+--A
+SELECT COUNT(role), role FROM employees GROUP BY role;
+--B
+SELECT COUNT(role), COUNT(phone), role FROM employees GROUP BY role;
+
+-- 3
+--A
+SELECT COUNT(item) AS "items", category AS "mammals" FROM stock_items WHERE category != "piscine" GROUP BY category;
+--B
+SELECT SUM(inventory) AS "in-stock", category AS "animal" FROM stock_items GROUP BY category ORDER BY SUM(inventory) ASC;
+--C
+SELECT MAX(price), category FROM stock_items GROUP BY category ORDER BY MAX(price) DESC;
+--D
+SELECT MAX(price) AS "item /highest price", category FROM stock_items GROUP BY category DESC HAVING MAX(price) > 50;
