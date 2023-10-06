8c49deec851ecf94139ca153f7418bb56511c453
jaalle1 <bbo3063@gmail.com>
2023-10-06 11:09:55 -0400

    added name to file

--- mysql/assignment4 sql/DB-Assign_4-Grouping_Results_EderesGure.sql
+++ mysql/assignment4 sql/DB-Assign_4-Grouping_Results_EderesGure.sql
@@ -1,4 +1,4 @@
---YOUR NAME HERE	ASSIGNMENT 4 GROUPING RESULTS
+--EDERES GURE	ASSIGNMENT 4 GROUPING RESULTS
 --Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 5; your query for question 1B should go on line 7...
 -- 1 
 --A
