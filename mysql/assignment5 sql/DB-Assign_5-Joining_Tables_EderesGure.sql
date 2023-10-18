5eb9cf58211239783224618c0d3473718883eb2c
jaalle1 <bbo3063@gmail.com>
2023-10-18 14:48:59 -0400

    fixed assignment5 sql

--- mysql/assignment5 sql/DB-Assign_5-Joining_Tables_EderesGure.sql
+++ mysql/assignment5 sql/DB-Assign_5-Joining_Tables_EderesGure.sql
@@ -7,7 +7,7 @@ SELECT sales.date , stock_items.item FROM sales JOIN stock_items ON sales.item =
 SELECT sales.item, stock_items.item, stock_items.category, sales.employee FROM sales INNER JOIN stock_items ON stock_items.id = sales.item WHERE stock_items.id = 1003; 
 --2
 -- A
-SELECT s.date, e.first_name, e.last_name, s.item FROM sales s JOIN employees e ON s.employee = e.id WHERE employee = 114; 
+SELECT s.date, e.first_name, e.last_name, s.item FROM sales s JOIN employees e ON s.employee = e.id WHERE employee = 111; 
 -- B
 SELECT e.first_name, e.last_name, e.role, e.sin, s.item FROM employees e RIGHT JOIN sales s ON e.id = s.employee WHERE e.sin LIKE "258%" OR e.sin LIKE "456%" OR e.sin LIKE "758%"; 
 
@@ -15,7 +15,7 @@ SELECT e.first_name, e.last_name, e.role, e.sin, s.item FROM employees e RIGHT J
 -- A
 SELECT s.date, s.item, e.first_name FROM sales s JOIN employees e ON s.employee = e.id WHERE date BETWEEN "2021-06-12" AND "2021-06-18"; 
 -- B
-SELECT concat(employees.first_name, " ", employees.last_name), COUNT(sales.id) FROM sales JOIN employees ON sales.employee = employees.id GROUP BY employees.first_name, employees.last_name ORDER BY COUNT(sales.id) DESC; 
+SELECT concat(employees.first_name, " ", employees.last_name) AS "sales person", COUNT(sales.id) FROM sales JOIN employees ON sales.employee = employees.id GROUP BY employees.first_name, employees.last_name ORDER BY COUNT(sales.id) DESC; 
 --4
 -- A
 SELECT s.date, k.item, k.price, k.category, e.first_name FROM sales s LEFT JOIN employees e ON s.employee = e.id LEFT JOIN stock_items k ON s.item = k.id WHERE first_name = "Farud"; 
