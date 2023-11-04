41e3cf6a60fcd1ca56b7d444b64f2f791b51aea0
jaalle1 <bbo3063@gmail.com>
2023-11-04 15:20:07 -0400

    done assignment7 sql expect 3B

--- mysql/assignment7 sql/DB-Assign_7-database-design-II_EderesGure.sql
+++ mysql/assignment7 sql/DB-Assign_7-database-design-II_EderesGure.sql
@@ -7,7 +7,7 @@ CREATE TABLE authors (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(100),
-    username VARCHAR(100),
+    username VARCHAR(100)
 );
 
 CREATE TABLE posts (
@@ -28,22 +28,21 @@ VALUES
 INSERT INTO posts (post_id, title, post_content, author_id, date_published)
 VALUES 
     (1, "Top 50 NES Games", "Here are the 50 best games on Nintendo's first console.", 2, "2023-10-30 08:00:00"),
-    (2, "DOOM: Eternal Review on PC", "Bethesda's sequel to 2016's DOOM is extra gory one." 3, "2020-03-15 12:35:21"),
-    (3, "Our Favourite Gaming Secrets", "Easter eggs as they are called are as old as gaming itself, here are our favourites." 1, "2014-05-17 15:43:05");
+    (2, "DOOM: Eternal Review on PC", "Bethesda's sequel to 2016's DOOM is extra gory one.", 3, "2020-03-15 12:35:21"),
+    (3, "Our Favourite Gaming Secrets", "Easter eggs as they are called are as old as gaming itself, here are our favourites.", 1, "2014-05-17 15:43:05");
 
 -- 2
 --A
-CREATE TABLE Comments (
+CREATE TABLE comments (
     comment_id INT PRIMARY KEY AUTO_INCREMENT,
     post_id INT,
     author_id INT,
     comment_text TEXT,
-    time_posted DATETIME,
     FOREIGN KEY (post_id) REFERENCES posts(post_id),
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 --B
-INSERT INTO Comments (post_id, author_id, comment_text)
+INSERT INTO comments (post_id, author_id, comment_text)
 VALUES 
     (1,2, "Yup, Super Mario Bros. 3 is the best game on the NES."),
     (2,3, "LMAO YOU GAVE IT 7?? DO YOU GUYS EVEN KNOW GAMING??"),
@@ -56,9 +55,17 @@ ADD comment_date DATETIME;
     (1,2, "Yup, Super Mario Bros. 3 is the best game on the NES.", "2023-10-30 11:41:30"),
     (2,3, "LMAO YOU GAVE IT 7?? DO YOU GUYS EVEN KNOW GAMING??", "2020-03-15 13:01:45"),
     (3,1, "Discovering the konami code as a kid was mind-blowing lol", "2014-05-17 20:32:15");
+
+    UPDATE comments set comment_date = '2023-10-30 11:41:30' WHERE comment_id = 1;
+    UPDATE comments set comment_date = '2020-03-15 13:01:45' WHERE comment_id = 2;
+    UPDATE comments set comment_date = '2014-05-17 20:32:15' WHERE comment_id = 3;
 --B
-DELETE authors, posts
+DELETE authors, posts, comments
 FROM authors
-LEFT JOIN posts ON authors.author_id = posts.author_id
-ORDER BY authors.author_id
-LIMIT 2;
+LEFT JOIN posts ON authors.author_id = posts.author_id, comments on authors.author_id = comments.author_id
+WHERE author_id IN (1,2);
+
+DELETE authors, posts, comments
+FROM authors
+INNER JOIN posts ON authors.author_id = posts.author_id INNER JOIN comments ON authors.author_id = comments.author_id
+WHERE authors.author_id IN (1,2);
